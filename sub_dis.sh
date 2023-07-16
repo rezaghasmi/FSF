28e166001e0e9dd5b8e2718c5a5c1bf8b9bcbe52
rezaghasmi <45834249+rezaghasmi@users.noreply.github.com>
2023-07-16 03:04:57 -1200

    Add files via upload

---
+++ sub_dis.sh
@@ -0,0 +1,8 @@
+for i in $(python3 word_ls.py $1);do echo "$i.$2";done | /root/go/bin/./dnsx -silent > comfz_$2
+
+/root/go/bin/./subfinder -d $2 -silent -all >  tools_$2.txt
+
+/root/go/bin/./comx  -silent  -f1  tools_$2.txt -f2 comfz_$2
+
+
+
